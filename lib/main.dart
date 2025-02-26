8bc83be88dc12ce8f2afff97632c6d2cade22a48
GAEL_ROGELIO_BEJARANO_DOMINGUEZ <a.22308051281154@cbtis128.edu.mx>
2025-02-26 20:41:32 +0000

    eee

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,100 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Iconos con Subtítulos',
+            style: TextStyle(fontSize: 27, color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Color(0xffff88ff),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Rogelio_Bejarano_22308051281154",
+                  style: TextStyle(fontSize: 22, color: Color(0xffc30c0c))),
+
+              // Primera fila
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconWithLabel(
+                    icon: Icons.home,
+                    label: '22308051281154',
+                    color: Colors.blue, // Color personalizado
+                  ),
+                  IconWithLabel(
+                    icon: Icons.search,
+                    label: '22308051281154',
+                    color: Colors.red, // Color personalizado
+                  ),
+                  IconWithLabel(
+                    icon: Icons.accessibility_new,
+                    label: '22308051281154',
+                    color: Colors.purple, // Color personalizado
+                  ),
+                ],
+              ),
+              SizedBox(height: 20), // Espacio entre filas
+              // Segunda fila
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconWithLabel(
+                    icon: Icons.settings,
+                    label: 'Rogelio Bejarano',
+                    color: Colors.green, // Color personalizado
+                  ),
+                  IconWithLabel(
+                    icon: Icons.directions_railway_filled_sharp,
+                    label: 'Rogelio Bejarano',
+                    color: Colors.orange, // Color personalizado
+                  ),
+                  IconWithLabel(
+                    icon: Icons.format_underlined_sharp,
+                    label: 'Rogelio Bejarano',
+                    color: Colors.teal, // Color personalizado
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class IconWithLabel extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final Color color; // Parámetro para el color
+
+  IconWithLabel({
+    required this.icon,
+    required this.label,
+    this.color = Colors.black, // Color predeterminado (negro)
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return Column(
+      children: [
+        Icon(icon, size: 40, color: color), // Aplicar el color al icono
+        SizedBox(height: 8), // Espacio entre el icono y el texto
+        Text(label),
+      ],
     );
   }
 }
